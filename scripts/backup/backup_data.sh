728a267acf62cfa5f2a3bba787face5544bea603
Kriticos <kriticos@gmail.com>
2026-04-18 23:01:15 -0300

    ci: corrige alertas do ShellCheck

--- scripts/backup/backup_data.sh
+++ scripts/backup/backup_data.sh
@@ -2,22 +2,21 @@
 
 # Variáveis
 data_atual=$(date +"%Y-%m-%d_%H-%M-%S")
-diretorios_origem=("/bskp/docker" "/bskp/grafana" "/bskp/scripts" "/bskp/zabbix" "/bskp/mysql")  
+diretorios_origem=("/bskp/docker" "/bskp/grafana" "/bskp/scripts" "/bskp/zabbix" "/bskp/README.md" "/bskp/mysql")
 diretorio_destino="/media/kriticos/bskp/bkp/bskp"
 
 # Criar backup para cada diretório
 echo "Iniciando backup das pastas selecionadas..."
 for origem in "${diretorios_origem[@]}"; do
-    nome_pasta=$(basename "$origem")  # Obtém apenas o nome da pasta
-    destino_pasta="${diretorio_destino}/${nome_pasta}"  # Define a pasta de destino
-    arquivo_bkp="${destino_pasta}/bkp_${nome_pasta}_${data_atual}.tar.gz"  # Nome do arquivo de backup
+    nome_pasta=$(basename "$origem")
+    destino_pasta="${diretorio_destino}/${nome_pasta}"
+    arquivo_bkp="${destino_pasta}/bkp_${nome_pasta}_${data_atual}.tar.gz"
 
     # Criar a pasta de destino, caso não exista
     mkdir -p "$destino_pasta"
 
     # Criar o backup
-    tar -czf "$arquivo_bkp" "$origem"
-    if [ $? -eq 0 ]; then
+    if tar -czf "$arquivo_bkp" "$origem"; then
         echo "Backup concluído para: $origem -> $arquivo_bkp"
     else
         echo "Erro ao realizar o backup de $origem"
